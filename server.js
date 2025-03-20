6268efa371ef9278b7e43e5c76122bab20cd5474
Vinicius Valverde <vinicius.valverde@aluno.senai.br>
2025-03-20 16:24:28 -0300

    feat: create project

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import router from './src/routes/gamesRoutes.js';
+
+const app = express();
+
+app.use(express.json());
+app.use('/games', router);
+
+const serverPort = process.env.PORT || 3000;
+
+app.listen(serverPort, () => {
+    console.log(`Servidor iniciado e rodando na porta ${serverPort}`);
+});
